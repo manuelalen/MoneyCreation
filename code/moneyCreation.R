cda45a06cd6716d2a248d2e99bad47677e62e714
Manuel Alén Sánchez <97231769+manuelalen@users.noreply.github.com>
2022-06-25 13:54:53 +0200

    Update moneyCreation.R

--- code/moneyCreation.R
+++ code/moneyCreation.R
@@ -10,7 +10,7 @@ createMoney <- function(fecha,item,dineroM){
   
   creationMoney <- dbGetQuery(database,statement = printingMoney)
 }
-#createMoney <- dbGetQuery(database,statement = "insert into tesoro_publico.activo_tesoro(dia,concepto,dinero_millones) values('01/01/2023','Tarjetas de Transporte',-(22826320*35*12)/1000000);")
+
 
 createMoney(fecha="01/01/2023",item="Salarios Trabajo Garantizado",dineroM = 104034.00)
 
